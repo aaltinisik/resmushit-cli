528f33eb944ae1954cc862078c0aa534ab9fdfad
Ahmet Altinisik <aaltinisik@altinkaya.com.tr>
2019-05-20 15:44:28 +0300

    changed filenames ad added find script

---
+++ image-optimize.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+cd /opt/prestashop/prestashop/
+find $(pwd) -iname "*.jpg" -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.jpeg" -o -iname "*.gif" -o -iname "*.bmp" -o -iname "*.tif"  \)   -print0 | \
+    (while read -d $'\0' i; do /opt/prestashop/resmushit-cli/resmushit-cli.sh "$i" --preserve-filename;  done)
+
+
