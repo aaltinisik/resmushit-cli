be46949eb3d84ae1072d457432837f3b45189312
root <root@host.altinkaya.com.tr>
2019-05-21 09:45:39 +0000

    updated

--- image-optimize.sh
+++ image-optimize.sh
@@ -2,6 +2,6 @@
 
 cd /opt/prestashop/prestashop/
 find $(pwd) -iname "*.jpg" -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.jpeg" -o -iname "*.gif" -o -iname "*.bmp" -o -iname "*.tif"  \)   -print0 | \
-    (while read -d $'\0' i; do /opt/prestashop/resmushit-cli/resmushit-cli.sh "$i" --preserve-filename;  done)
+    (while read -d $'\0' i; do /opt/resmushit-cli/resmushit-cli.sh "$i" --preserve-filename -o "$(dirname "$i")" ;  done)
 
 
